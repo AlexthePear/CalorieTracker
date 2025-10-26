9f1e24326e679fde0867183e224350ccf9d60527
AlexthePear <alop5570@gmail.com>
2025-10-26 05:20:08 -0700

    redeploy

--- backend/main.py
+++ backend/main.py
@@ -27,7 +27,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://localhost:5173",
-        "https://localhost:5000"
+        "http://localhost:5000"
     ],
     allow_credentials=True,
     allow_methods=["*"],
